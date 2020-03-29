f9935b8f600475f399c60af7be7966daa97a9c0b
Junior <junnior.ferreira13@hotmail.com>
2020-03-29 11:12:28 -0300

    Mobile app and unit/integration test

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express'); // routes
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express(); // Create appliaction
@@ -7,5 +8,6 @@ const app = express(); // Create appliaction
 app.use(cors());
 app.use(express.json()); // convert json after request to javascript object
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
